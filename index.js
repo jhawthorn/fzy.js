f7d00723a8f8959bd4dcb0f1cf23303bffcc161c
John Hawthorn <john@hawthorn.email>
2018-11-03 17:07:37 -0700

    Translate implementation from fzy

---
+++ index.js
@@ -0,0 +1,113 @@
+var SCORE_MIN = -Infinity;
+var SCORE_MAX = Infinity;
+
+var SCORE_GAP_LEADING = -0.005
+var SCORE_GAP_TRAILING = -0.005
+var SCORE_GAP_INNER = -0.01
+var SCORE_MATCH_CONSECUTIVE = 1.0
+var SCORE_MATCH_SLASH = 0.9
+var SCORE_MATCH_WORD = 0.8
+var SCORE_MATCH_CAPITAL = 0.7
+var SCORE_MATCH_DOT = 0.6
+
+function islower(s) {
+	s.toLowerCase() === s;
+}
+
+function isupper(s) {
+	s.toUpperCase() === s;
+}
+
+function precompute_bonus(haystack) {
+	/* Which positions are beginning of words */
+	var m = haystack.length;
+	var match_bonus = new Array(m);
+
+	var last_ch = '/';
+	for (var i = 0; i < m; i++) {
+		var ch = haystack[i];
+
+		if (last_ch === '/') {
+			match_bonus[i] = SCORE_MATCH_SLASH;
+		} else if (last_ch === '-' || last_ch === '_' || last_ch === ' ') {
+			match_bonus[i] = SCORE_MATCH_WORD;
+		} else if (last_ch === '.') {
+			match_bonus[i] = SCORE_MATCH_DOT;
+		} else if (islower(last_ch) && isupper(ch)) {
+			match_bonus[i] = SCORE_MATCH_CAPITAL;
+		} else {
+			match_bonus[i] = 0;
+		}
+
+		last_ch = ch;
+	}
+
+	return match_bonus;
+}
+
+function score(needle, haystack) {
+	var n = needle.length;
+	var m = haystack.length;
+
+	if (!n || !m)
+		return SCORE_MIN;
+
+	if (n == m) {
+		/* Since this method can only be called with a haystack which
+		 * matches needle. If the lengths of the strings are equal the
+		 * strings themselves must also be equal (ignoring case).
+		 */
+		return SCORE_MAX;
+	}
+
+	if (m > 1024) {
+		/*
+		 * Unreasonably large candidate: return no score
+		 * If it is a valid match it will still be returned, it will
+		 * just be ranked below any reasonably sized candidates
+		 */
+		return SCORE_MIN;
+	}
+
+	var lower_needle = needle.toLowerCase();
+	var lower_haystack = haystack.toLowerCase();
+
+	var D = new Array(n);
+	var M = new Array(n);
+
+	/*
+	 * D[][] Stores the best score for this position ending with a match.
+	 * M[][] Stores the best possible score at this position.
+	 */
+	var match_bonus = precompute_bonus(haystack, match_bonus);
+
+	for (var i = 0; i < n; i++) {
+		D[i] = new Array(m);
+		M[i] = new Array(m);
+
+		var prev_score = SCORE_MIN;
+		var gap_score = i == n - 1 ? SCORE_GAP_TRAILING : SCORE_GAP_INNER;
+
+		for (var j = 0; j < m; j++) {
+			if (lower_needle[i] == lower_haystack[j]) {
+				var score = SCORE_MIN;
+				if (!i) {
+					score = (j * SCORE_GAP_LEADING) + match_bonus[j];
+				} else if (j) { /* i > 0 && j > 0*/
+					score = Math.max(
+						M[i - 1][j - 1] + match_bonus[j],
+
+						/* consecutive match, doesn't stack with match_bonus */
+						D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE);
+				}
+				D[i][j] = score;
+				M[i][j] = prev_score = Math.max(score, prev_score + gap_score);
+			} else {
+				D[i][j] = SCORE_MIN;
+				M[i][j] = prev_score = prev_score + gap_score;
+			}
+		}
+	}
+
+	return M[n - 1][m - 1];
+}
